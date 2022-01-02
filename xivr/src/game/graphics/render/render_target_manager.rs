b3b688630efa654161521f46384c28a7a872e6bc
Philpax <me@philpax.me>
2022-01-02 12:40:15 +0100

    xivr: replace get_render_targets with fields

--- xivr/src/game/graphics/render/render_target_manager.rs
+++ xivr/src/game/graphics/render/render_target_manager.rs
@@ -6,60 +6,84 @@ game_class!(RenderTargetManager, {
     size: 0x3D8,
     location: offsets::classes::graphics::render::RenderTargetManager::INSTANCES[0],
     fields: {
-        [0x58] rendered_game: &'static Texture,
-
-        // specific ones i can name
-        // offscreen renderer is used to render models for UI elements like the character window
-        [0x1E0] offscreen_render_target_1: &'static Texture,
-        [0x1E8] offscreen_render_target_2: &'static Texture,
-        [0x1F0] offscreen_render_target_3: &'static Texture,
-        [0x1F8] offscreen_render_target_4: &'static Texture,
-        [0x200] offscreen_g_buffer: &'static Texture,
-        [0x208] offscreen_depth_stencil: &'static Texture,
-
-        // these are related to offscreen renderer due to their size
-        [0x210] offscreen_render_target_unk1: &'static Texture,
-
-        [0x218] offscreen_render_target_unk2: &'static Texture,
-        [0x220] offscreen_render_target_unk3: &'static Texture,
-
-        [0x228] resolution_width: u32,
-        [0x22C] resolution_height: u32,
-        [0x230] shadow_map_width: u32,
-        [0x234] shadow_map_height: u32,
-        [0x238] near_shadow_map_width: u32,
-        [0x23C] near_shadow_map_height: u32,
-        [0x240] far_shadow_map_width: u32,
-        [0x244] far_shadow_map_height: u32,
-        [0x248] unk_bool_1: bool,
-
-        [0x358] some_rgb8_buffer: &'static Texture,
+        [0x070] texture_070: *mut Texture,
+        [0x098] texture_098: *mut Texture,
+        [0x0B8] texture_0B8: *mut Texture,
+        [0x0C0] texture_0C0: *mut Texture,
+        [0x0C8] texture_0C8: *mut Texture,
+        [0x0D0] texture_0D0: *mut Texture,
+        [0x0F8] texture_0F8: *mut Texture,
+        [0x160] texture_160: *mut Texture,
+        [0x168] texture_168: *mut Texture,
+        [0x170] texture_170: *mut Texture,
+        [0x190] texture_190: *mut Texture,
+        [0x1B0] texture_1B0: *mut Texture,
+        [0x1C8] texture_1C8: *mut Texture,
+        [0x1D8] texture_1D8: *mut Texture,
+        [0x1E0] texture_1E0: *mut Texture,
+        [0x200] texture_200: *mut Texture,
+        [0x208] texture_208: *mut Texture,
+        [0x210] texture_210: *mut Texture,
+        [0x218] texture_218: *mut Texture,
+        [0x220] texture_220: *mut Texture,
+        [0x368] texture_368: *mut Texture,
+        [0x370] texture_370: *mut Texture,
+        [0x378] texture_378: *mut Texture,
+        [0x380] texture_380: *mut Texture,
+        [0x388] texture_388: *mut Texture,
+        [0x390] texture_390: *mut Texture,
+        [0x398] texture_398: *mut Texture,
+        [0x3A0] texture_3A0: *mut Texture,
+        [0x3A8] texture_3A8: *mut Texture,
+        [0x3B0] texture_3B0: *mut Texture,
+        [0x3B8] texture_3B8: *mut Texture,
+        [0x3C0] texture_3C0: *mut Texture,
+        [0x3C8] texture_3C8: *mut Texture,
+        [0x3D0] texture_3D0: *mut Texture,
     }
 });
 
 impl RenderTargetManager {
-    pub fn get_render_targets(&self) -> Vec<(u32, *const Texture)> {
-        let offsets = [
-            0x20, 0x58, 0x60, 0x68, 0xB0, 0xB8, 0xC0, 0xC8, 0xD0, 0xF8, 0x160, 0x170, 0x1C8, 0x1D8,
-            0x210, 0x218, 0x220, 0x2D0, 0x358, 0x368, 0x370, 0x378, 0x380, 0x388, 0x390, 0x398,
-            0x3A0, 0x3A8, 0x3B0, 0x3B8, 0x3C0, 0x3C8, 0x3D0,
-        ];
+    pub fn get_render_targets(&self) -> Vec<(u32, *mut Texture)> {
+        let textures = unsafe {
+            vec![
+                (0x070, *self.texture_070()),
+                (0x098, *self.texture_098()),
+                (0x0B8, *self.texture_0B8()),
+                (0x0C0, *self.texture_0C0()),
+                (0x0C8, *self.texture_0C8()),
+                (0x0D0, *self.texture_0D0()),
+                (0x0F8, *self.texture_0F8()),
+                (0x160, *self.texture_160()),
+                (0x168, *self.texture_168()),
+                (0x170, *self.texture_170()),
+                (0x190, *self.texture_190()),
+                (0x1B0, *self.texture_1B0()),
+                (0x1C8, *self.texture_1C8()),
+                (0x1D8, *self.texture_1D8()),
+                (0x1E0, *self.texture_1E0()),
+                (0x200, *self.texture_200()),
+                (0x208, *self.texture_208()),
+                (0x210, *self.texture_210()),
+                (0x218, *self.texture_218()),
+                (0x220, *self.texture_220()),
+                (0x368, *self.texture_368()),
+                (0x370, *self.texture_370()),
+                (0x378, *self.texture_378()),
+                (0x380, *self.texture_380()),
+                (0x388, *self.texture_388()),
+                (0x390, *self.texture_390()),
+                (0x398, *self.texture_398()),
+                (0x3A0, *self.texture_3A0()),
+                (0x3A8, *self.texture_3A8()),
+                (0x3B0, *self.texture_3B0()),
+                (0x3B8, *self.texture_3B8()),
+                (0x3C0, *self.texture_3C0()),
+                (0x3C8, *self.texture_3C8()),
+                // (0x3D0, *self.texture_3D0()),
+            ]
+        };
 
-        let mut ret = vec![];
-        unsafe {
-            let self_ptr = self as *const RenderTargetManager;
-            let self_ptr = self_ptr as *const u8;
-
-            for offset in offsets {
-                let rt_ptr = self_ptr.add(offset) as *const *const Texture;
-                let rt = *rt_ptr;
-                if rt.is_null() {
-                    continue;
-                }
-                ret.push((offset as u32, rt));
-            }
-        }
-
-        ret
+        textures.into_iter().filter(|(_, p)| !p.is_null()).collect()
     }
 }
