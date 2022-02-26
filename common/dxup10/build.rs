e2e4a1c40607cd4c5e13df2d67e1f27c5fba5895
Philpax <me@philpax.me>
2022-02-26 01:19:25 +0100

    dxup10: build as dylib (this doesn't actually work)

--- common/dxup10/build.rs
+++ common/dxup10/build.rs
@@ -8,15 +8,16 @@ fn main() {
         paths.push(path);
     }
 
-    for entry in glob("src-cpp/d3d11/*.cpp").expect("Failed to read glob pattern") {
-        let path = entry.unwrap();
-        paths.push(path);
-    }
-
     cc::Build::new()
         .cpp(true)
         .files(paths)
         .flag("/std:c++17")
         .define("NOMINMAX", None)
+        .define("_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING", None)
+        .flag("/wd4100") // unreferenced formal parameter
+        .flag("/wd4459") // hides global declaration
+        .flag("/wd4267") // possible loss of data
+        .object("d3dcompiler.lib")
+        .object("dxgi.lib")
         .compile("libdxup10");
 }
