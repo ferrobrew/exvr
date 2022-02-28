c33a1263232e0190c8960d61a287808a7c973456
Philpax <me@philpax.me>
2022-02-28 22:57:15 +0100

    dxup-rs: define _DEBUG if building in debug

--- jc2vr/dxup-rs/build.rs
+++ jc2vr/dxup-rs/build.rs
@@ -7,31 +7,38 @@ fn main() -> anyhow::Result<()> {
     }
 
     println!("cargo:rerun-if-changed=src-cpp");
-    let paths: Vec<_> = std::iter::once("src-cpp/dxup_guids.cpp".into())
-        .chain(glob("src-cpp/d3d10_1/*.cpp")?.filter_map(Result::ok))
-        .chain(glob("src-cpp/dxgi/*.cpp")?.filter_map(Result::ok))
-        .collect();
-
-    cc::Build::new()
+    let mut build = cc::Build::new();
+    build
         .cpp(true)
         // files
         .include("src-cpp/d3d10_1") // necessary for dxgi
-        .files(paths)
+        .files(
+            std::iter::once("src-cpp/dxup_guids.cpp".into())
+                .chain(glob("src-cpp/d3d10_1/*.cpp")?.filter_map(Result::ok))
+                .chain(glob("src-cpp/dxgi/*.cpp")?.filter_map(Result::ok)),
+        )
         // compiler flags
         .flag("/std:c++17")
-        .define("DXUP_EXPORTS", None)
-        .define("DXUP_DXGI_EXPORTS", None)
-        .define("NOMINMAX", None)
-        .define(
-            "_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING",
-            None,
-        )
         .flag("/wd4100") // unreferenced formal parameter
         .flag("/wd4459") // hides global declaration
         .flag("/wd4267") // possible loss of data
-        .flag("/wd5205") // delete of an abstract class
-        // linker
-        .compile("libdxup");
+        .flag("/wd5205"); // delete of an abstract class
+
+    let is_debug = std::env::var("PROFILE") == Ok("debug".to_string());
+    let mut defines = vec![
+        "DXUP_EXPORTS",
+        "DXUP_DXGI_EXPORTS",
+        "NOMINMAX",
+        "_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING",
+    ];
+    if is_debug {
+        defines.push("_DEBUG");
+    }
+    for define in defines {
+        build.define(define, None);
+    }
+
+    build.compile("libdxup");
 
     Ok(())
 }
