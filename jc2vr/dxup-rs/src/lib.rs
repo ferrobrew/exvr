afd065295aefbae0346cef3478b8d92c19bbc95e
Philpax <me@philpax.me>
2022-02-28 22:22:14 +0100

    dxup-rs: patch out Nvidia-specific device creation

--- jc2vr/dxup-rs/src/lib.rs
+++ jc2vr/dxup-rs/src/lib.rs
@@ -7,18 +7,19 @@ mod util;
 
 use std::os::raw::c_void;
 
+use anyhow::Context;
 use windows_sys::Win32::Foundation::HINSTANCE;
 
-fn safe_patch(patch_address: *mut *const (), new_address: *const ()) {
+fn safe_write<T>(address: *mut T, value: T) {
     use windows_sys::Win32::System::Memory::{
         VirtualProtect, PAGE_PROTECTION_FLAGS, PAGE_READWRITE,
     };
     let mut old: PAGE_PROTECTION_FLAGS = 0;
-    let size = std::mem::size_of_val(&new_address) as usize;
+    let size = std::mem::size_of_val(&value) as usize;
     unsafe {
-        VirtualProtect(patch_address as *const _, size, PAGE_READWRITE, &mut old);
-        *patch_address = new_address;
-        VirtualProtect(patch_address as *const _, size, old, &mut old);
+        VirtualProtect(address as *const _, size, PAGE_READWRITE, &mut old);
+        *address = value;
+        VirtualProtect(address as *const _, size, old, &mut old);
     }
 }
 
@@ -36,7 +37,7 @@ fn iat_hook() -> anyhow::Result<()> {
     for (library_name, functions) in import_tables().filter(|(l, _)| dlls.contains(l.as_str())) {
         for (function_name, ptr_to_function) in functions {
             if let Some(address) = functions_by_name.get(&(&library_name, &function_name)) {
-                safe_patch(ptr_to_function, *address);
+                safe_write(ptr_to_function, *address);
             }
         }
     }
@@ -74,13 +75,23 @@ fn jc2_direct_hook() -> anyhow::Result<()> {
         DETOURS = Some(detours);
     }
 
+    // JC2 attempts to call Nvidia-specific device creation if it detects a Nvidia card.
+    // We don't want that, so let's change the conditional jump to an unconditional one.
+    safe_write(0x9385C7 as *mut _, 0xEBu8);
+    // Similarly, disable stereo vision.
+    safe_write(0x938FC8 as *mut _, 0xEBu8);
+
     Ok(())
 }
 
 fn load_impl() -> anyhow::Result<()> {
-    const USE_DIRECT_HOOK: bool = true;
+    let exe_filename = std::env::current_exe()?
+        .file_name()
+        .context("failed to get filename")?
+        .to_string_lossy()
+        .to_ascii_lowercase();
 
-    if USE_DIRECT_HOOK {
+    if exe_filename == "justcause2.exe" {
         jc2_direct_hook()
     } else {
         iat_hook()
